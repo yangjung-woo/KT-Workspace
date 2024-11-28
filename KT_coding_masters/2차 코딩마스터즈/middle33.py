41431d37a0df8b08498a072d50ec2b9ca4941bb9
yee777222000@gmail.com <yee777222000@gmail.com>
2024-11-28 15:24:53 +0900

    불꽃놀이 (카데인 알고리즘 )

--- KT_coding_masters/2차 코딩마스터즈/middle33.py
+++ KT_coding_masters/2차 코딩마스터즈/middle33.py
@@ -1,16 +1,25 @@
-# 문제 접근 N 은 최대 5000 , O(N**2) 까지는 시간초과 X 
-# 완전탐색(브루트포스 탐색)
+# 문제 접근 N 은 최대 5000 , O(N**2) 시간초과 발생 예상
+# 완전탐색(브루트포스 탐색) >> 카데인 알고리즘 필요(최대 구간 합 계산에 유용)
+
+
 
 N = int(input())
 
 moneys = list(map(int,input().split()))
 
-dp = moneys.copy() # 메모리제이션용 배열 (최대 이득만 저장)
+#dp = moneys.copy() # 메모리제이션용 배열 (최대 이득만 저장)
+# 완전 탐색
+# for i in range(N):
+#     for j in range(i+1,N):
+#         dp[i] = max(moneys[i], sum(moneys[i:j+1]))
 
-for i in range(N):
-    for j in range(i+1,N):
-        dp[i] = max(moneys[i], sum(moneys[i:j+1]))
-    
-print(max(dp))
+# 카데인 알고리즘 
+max_current = moneys[0]
+max_global = moneys[0]
+
+for i in range(1, N):
+    max_current = max(moneys[i], max_current + moneys[i])  # 현재까지 최대값 갱신
+    max_global = max(max_global, max_current)  # 전체 최대값 갱신
+print(max_global)
     
     
