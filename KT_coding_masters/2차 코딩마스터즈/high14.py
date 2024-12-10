bb16e681d8bdc2a2065388fd7ac415adbed57a1e
yee777222000@gmail.com <yee777222000@gmail.com>
2024-12-10 10:00:06 +0900

    창틀제작 : 나머지 조건 추가

--- KT_coding_masters/2차 코딩마스터즈/high14.py
+++ KT_coding_masters/2차 코딩마스터즈/high14.py
@@ -2,19 +2,24 @@
 # i 번째 칸에서 최대 jump[i]만큼 점프할 수 있다 
 # i +1 ~ i +jump[i] 까지의 칸 j 는    dp[i]를 추가적으로 더한다 
 #  dp[j]=dp[j]+dp[i], (i+1≤j≤i+jumps[i])
-MOD = 1000000007
 
+def count_jump_paths(N, jumps):
+    MOD = 1000000007
+
+    # dp[i]: i번째 칸에 도달하는 경로의 수
+    dp = [0] * N
+    dp[0] = 1  # 시작점 초기화
+
+    for i in range(N):
+        max_jump = jumps[i]
+        for j in range(i + 1, min(i + max_jump + 1, N)):
+            dp[j] = (dp[j] + dp[i]) % MOD
+
+    return dp[-1]
+
+# 입력 받기
 N = int(input())
 jumps = list(map(int, input().split()))
 
-# dp[i]: i번째 칸에 도달하는 경로의 수
-dp = [0] * N
-dp[0] = 1  # 시작점 초기화
-
-for i in range(N):
-    max_jump = jumps[i]
-    for j in range(i + 1, min(i + max_jump + 1, N)):
-        dp[j] = (dp[j] + dp[i]) % MOD
-
-
-print(dp[-1])
+# 결과 출력
+print(count_jump_paths(N, jumps))
--- KT_coding_masters/2차 코딩마스터즈/middle48.py
+++ KT_coding_masters/2차 코딩마스터즈/middle48.py
@@ -7,9 +7,13 @@ tobe_window = N  # 최소 필요한 창문
 
 
 # 1. 창틀을 최대한 확보
-cnt =  tobe_frame //(a-1) -1
-    
+cnt =  (tobe_frame-1) //(a-1)
+
+#(조건) 
+if (tobe_frame-1) %(a-1)!=0: # 나누어 떨어지지 않는 경우 
+    cnt +=1 
+
 # 2. 창문 제작 
-cnt = cnt + tobe_frame // b
+cnt = cnt + N
 
 print(cnt)
